72c7498d55c313ce53757a6f35aecd8aaa370805
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2024-10-19 19:27:49 +0300

    - merge splash android 12 screen

--- lib/feature/login/ui/login_screen.dart
+++ lib/feature/login/ui/login_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/theming/styles.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child:Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w , vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Login', style: TextStyles.font18WhiteBold,),
+              ]
+            ),
+          ),
+        )
+      ),
+    );
   }
 }  
